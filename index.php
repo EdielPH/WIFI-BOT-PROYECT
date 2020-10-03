83577757ba57925d5972fa37a68df2edeb066bac
EdielPH <ediel-ph@hotmail.com>
2020-10-03 03:07:48 -0600

    adicion al php

--- index.php
+++ index.php
@@ -16,10 +16,11 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-	<title>WIFIBOTPROYECT</title>
-	<link rel="icon" type="image/png" href="images/chip.png"/>
+     <title>WIFIBOTPROYECT</title>
+     <link rel="icon" type="image/png" href="images/chip.png" />
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
      <meta name="description" content="">
@@ -36,6 +37,7 @@
      <link rel="stylesheet" href="css/tooplate-style.css">
 
 </head>
+
 <body>
 
      <!-- PRE LOADER -->
@@ -43,7 +45,7 @@
           <div class="spinner">
 
                <span class="spinner-rotate"></span>
-               
+
           </div>
      </section>
 
@@ -60,7 +62,7 @@
                     </button>
 
                     <!-- lOGO TEXT HERE -->
-                    <a href="index.html" class="navbar-brand">WiFi-Bots Proyect</a>
+                    <a href="index.php" class="navbar-brand">WiFi-Bots Proyect</a>
                </div>
 
                <!-- MENU LINKS -->
@@ -90,7 +92,7 @@
                          <div class="home-info">
                               <h3>MECH ROBOTIX</h3>
                               <h1>Desarrolla habilidades tecnológicas!</h1>
-                              
+
                          </div>
                     </div>
 
@@ -112,9 +114,11 @@
 
                     <div class="col-md-6 col-sm-6">
                          <ul class="nav nav-tabs" role="tablist">
-                              <li class="active"><a href="#tab01" aria-controls="tab01" role="tab" data-toggle="tab">Descripción</a></li>
+                              <li class="active"><a href="#tab01" aria-controls="tab01" role="tab"
+                                        data-toggle="tab">Descripción</a></li>
 
-                              <li><a href="#tab02" aria-controls="tab02" role="tab" data-toggle="tab">Especificaciónes</a></li>
+                              <li><a href="#tab02" aria-controls="tab02" role="tab"
+                                        data-toggle="tab">Especificaciónes</a></li>
 
                               <li><a href="#tab03" aria-controls="tab03" role="tab" data-toggle="tab">Soporte</a></li>
                          </ul>
@@ -123,7 +127,11 @@
                               <div class="tab-pane active" id="tab01" role="tabpanel">
                                    <div class="tab-pane-item">
                                         <h2>Arduino WeMos D1 WiFi UNO ESP8266 IOT IDE Compatible Board</h2>
-                                        <p>Placa de desarrollo WiFi ESP8266 WEMOS D1. WEMOS D1 es una placa de desarrollo WIFI basada en ESP8266 12E. El funcionamiento es similar al de NODEMCU, excepto que el hardware está construido de forma similar a Arduino UNO. La placa D1 se puede configurar para trabajar en el entorno Arduino utilizando BOARDS MANAGER.</p>
+                                        <p>Placa de desarrollo WiFi ESP8266 WEMOS D1. WEMOS D1 es una placa de
+                                             desarrollo WIFI basada en ESP8266 12E. El funcionamiento es similar al de
+                                             NODEMCU, excepto que el hardware está construido de forma similar a Arduino
+                                             UNO. La placa D1 se puede configurar para trabajar en el entorno Arduino
+                                             utilizando BOARDS MANAGER.</p>
                                    </div>
                                    <div class="tab-pane-item">
                                         <h2>PROGRESS</h2>
@@ -156,11 +164,17 @@
                               <div class="tab-pane" id="tab03" role="tabpanel">
                                    <div class="tab-pane-item">
                                         <h2>Soporte rapido</h2>
-                                        <p>Hay una variante de esta misma placa con muchos menos pines pero las mismas posibilidades y de mucho menor tamaño ideal para pequeños proyectos que requieran poco tamaño y WIFI, que se llama WeMos D1 mini.</p>
+                                        <p>Hay una variante de esta misma placa con muchos menos pines pero las mismas
+                                             posibilidades y de mucho menor tamaño ideal para pequeños proyectos que
+                                             requieran poco tamaño y WIFI, que se llama WeMos D1 mini.</p>
                                    </div>
                                    <div class="tab-pane-item">
                                         <h2>A tomar en cuenta</h2>
-                                        <p>Aunque seguro que habrá cosas que fallarán, son unas placas de lo más curiosas y recomendables si queréis pelearos con los WIFI baratos y los ESP8266, os recomiendo que empecéis por aquí y os olvidéis inicialmente de ESP01 y demás. Ya tendréis tiempo de usar chips cuando estáis diseñando una placa integrada.</p>
+                                        <p>Aunque seguro que habrá cosas que fallarán, son unas placas de lo más
+                                             curiosas y recomendables si queréis pelearos con los WIFI baratos y los
+                                             ESP8266, os recomiendo que empecéis por aquí y os olvidéis inicialmente de
+                                             ESP01 y demás. Ya tendréis tiempo de usar chips cuando estáis diseñando una
+                                             placa integrada.</p>
                                    </div>
                               </div>
                          </div>
@@ -169,7 +183,7 @@
 
                     <div class="col-md-6 col-sm-6">
                          <div class="feature-image">
-                              <img src="images/D1.jpg" class="img-responsive" alt="Thin Laptop">                             
+                              <img src="images/D1.jpg" class="img-responsive" alt="Thin Laptop">
                          </div>
                     </div>
 
@@ -190,11 +204,12 @@
                     </div>
                     <div class="col-md-offset-3 col-md-6 col-sm-12">
                          <div class="image">
-                              <img src="images/progress.jpg" class="img-responsive" alt="Coming Soon">                          
+                              <img src="images/progress.jpg" class="img-responsive" alt="Coming Soon">
                          </div>
                     </div>
-          </div>
+               </div>
      </section>
+     
 
 
      <!-- TESTIMONIAL -->
@@ -209,44 +224,57 @@
                     <div class="col-md-6 col-sm-12">
                          <div class="testimonial-info">
                               
-                              <div class="section-title">
-                                   <h1>Mas proyectos</h1>
-                              </div>
+                              <!--<div class="section-title">
+                                   <h1>MECH ROBOTIX</h1>
+                              </div>-->
 
                               <div class="owl-carousel owl-theme">
                                    <div class="item">
-                                        <h3>Vestibulum tempor facilisis efficitur. Sed nec nisi sit amet nibh pellentesque elementum. In viverra ipsum ornare sapien rhoncus ullamcorper.</h3>
+                                        <h1>
+                                             Talleres de robótica
+                                        </h1>
+                                        <h3>
+                                             Aprenderás electrónica, programación, construirás tu propio robot que te llevas a tu casa.
+                                        </h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image1.jpg" class="img-responsive" alt="Ediel">
-                                             <h4>Ediel</h4>
+                                             <img src="images/robot2.png" class="img-responsive" alt="Robot">
+                                             <h4></h4>
                                         </div>
                                    </div>
 
                                    <div class="item">
-                                        <h3>Donec pretium tristique elit eget sodales. Pellentesque posuere, nunc id interdum venenatis, leo odio cursus sapien, ac malesuada nisl libero eget urna.</h3>
+                                        <h1>
+                                             Nuestros Alumnos
+                                        </h1>
+                                        <h3>
+                                             Contamos con alumnos ganadores a nivel Nacional y representantes de México en CONCURSOS INTERNACIONALES.
+                                        </h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image2.jpg" class="img-responsive" alt="Eddie">
-                                             <h4>Eddie</h4>
+                                             <img src="images/estudiar2.png" class="img-responsive" alt="Estudiantes">
+                                             <h4></h4>
                                         </div>
                                    </div>
 
                                    <div class="item">
-                                        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</h3>
+                                        <h1>
+                                             Horarios Flexibles
+                                        </h1>
+                                        <h3>Contamos con diferentes horarios de clases y talleres durante toda la semana para que todos puedan aprender.</h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image3.jpg" class="img-responsive" alt="Arthur">
-                                             <h4>Arthur</h4>
+                                             <img src="images/calendario2.png" class="img-responsive" alt="Horarios">
+                                             <h4></h4>
                                         </div>
                                    </div>
                               </div>
 
                          </div>
                     </div>
-                    
+
                </div>
           </div>
      </section>
 
-  
+
 
 
      <!-- CONTACT -->
@@ -260,22 +288,28 @@
                                    <h1>Mech Robotix Academia de Robótica y Tecnologías</h1>
                               </div>
                               <div>
-                                   <button type="button" class="btn btn-outline-success">Comencemos!</button>
+                                   <button type="button" class="btn btn-success">
+                                        <a href="remote-control.php">Comencemos!</a>
+                                   </button>
                               </div>
 
-                          
+
                          </form>
                     </div>
 
                </div>
           </div>
      </section>       
-
+     <div>
+          <hr color="green" size=1 width="1300">
+     </div>
 
      <!-- FOOTER -->
      <footer id="footer" data-stellar-background-ratio="0.5">
+
           <div class="container">
                <div class="row">
+
 
                     <div class="copyright-text col-md-12 col-sm-12">
                          <div class="col-md-6 col-sm-6">
@@ -305,4 +339,5 @@
      <script src="js/custom.js"></script>
 
 </body>
+
 </html>
