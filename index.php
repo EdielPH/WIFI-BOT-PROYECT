6d80078be1a82db930de0ab3e854842a282d1b4d
EdielPH <ediel-ph@hotmail.com>
2020-10-03 02:56:48 -0600

    Update index.php
    cambios previos del index.html a probar en el index.php

--- index.php
+++ index.php
@@ -195,6 +195,7 @@
                     </div>
           </div>
      </section>
+     
 
 
      <!-- TESTIMONIAL -->
@@ -209,32 +210,45 @@
                     <div class="col-md-6 col-sm-12">
                          <div class="testimonial-info">
                               
-                              <div class="section-title">
-                                   <h1>Mas proyectos</h1>
-                              </div>
+                              <!--<div class="section-title">
+                                   <h1>MECH ROBOTIX</h1>
+                              </div>-->
 
                               <div class="owl-carousel owl-theme">
                                    <div class="item">
-                                        <h3>Vestibulum tempor facilisis efficitur. Sed nec nisi sit amet nibh pellentesque elementum. In viverra ipsum ornare sapien rhoncus ullamcorper.</h3>
+                                        <h1>
+                                             Talleres de robótica
+                                        </h1>
+                                        <h3>
+                                             Aprenderás electrónica, programación, construirás tu propio robot que te llevas a tu casa.
+                                        </h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image1.jpg" class="img-responsive" alt="Ediel">
-                                             <h4>Ediel</h4>
+                                             <img src="images/robot2.png" class="img-responsive" alt="Robot">
+                                             <h4></h4>
                                         </div>
                                    </div>
 
                                    <div class="item">
-                                        <h3>Donec pretium tristique elit eget sodales. Pellentesque posuere, nunc id interdum venenatis, leo odio cursus sapien, ac malesuada nisl libero eget urna.</h3>
+                                        <h1>
+                                             Nuestros Alumnos
+                                        </h1>
+                                        <h3>
+                                             Contamos con alumnos ganadores a nivel Nacional y representantes de México en CONCURSOS INTERNACIONALES.
+                                        </h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image2.jpg" class="img-responsive" alt="Eddie">
-                                             <h4>Eddie</h4>
+                                             <img src="images/estudiar2.png" class="img-responsive" alt="Estudiantes">
+                                             <h4></h4>
                                         </div>
                                    </div>
 
                                    <div class="item">
-                                        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</h3>
+                                        <h1>
+                                             Horarios Flexibles
+                                        </h1>
+                                        <h3>Contamos con diferentes horarios de clases y talleres durante toda la semana para que todos puedan aprender.</h3>
                                         <div class="testimonial-item">
-                                             <img src="images/tst-image3.jpg" class="img-responsive" alt="Arthur">
-                                             <h4>Arthur</h4>
+                                             <img src="images/calendario2.png" class="img-responsive" alt="Horarios">
+                                             <h4></h4>
                                         </div>
                                    </div>
                               </div>
@@ -260,7 +274,7 @@
                                    <h1>Mech Robotix Academia de Robótica y Tecnologías</h1>
                               </div>
                               <div>
-                                   <button type="button" class="btn btn-outline-success">Comencemos!</button>
+                                   <button type="button" class="btn btn-success">Success</button>
                               </div>
 
                           
@@ -270,13 +284,17 @@
                </div>
           </div>
      </section>       
-
+     <div>
+          <hr color="green" size=1 width="1300">
+     </div>
 
      <!-- FOOTER -->
      <footer id="footer" data-stellar-background-ratio="0.5">
+          
           <div class="container">
                <div class="row">
-
+                   
+                    
                     <div class="copyright-text col-md-12 col-sm-12">
                          <div class="col-md-6 col-sm-6">
                               <p>Copyright &copy; Todos los Derechos Están Reservados.</p>
