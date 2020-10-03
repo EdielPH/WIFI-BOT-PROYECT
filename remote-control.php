7640889e4773d306190bc2c3b0d5c0d26c0114fb
EdielPH <ediel-ph@hotmail.com>
2020-10-03 02:57:55 -0600

    Create remote-control.php
    primera prueba de segunda seccion php

---
+++ remote-control.php
@@ -0,0 +1,156 @@
+
+<?php
+//$mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
+//if ($mysqli->connect_errno) {
+    //echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+//   }
+//echo $mysqli->host_info . "\n";
+
+//$mysqli = new mysqli("177.236.41.88", "usuario", "contraseña", "basedatos", 3306);
+//if ($mysqli->connect_errno) {
+    //echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+//}
+
+//echo $mysqli->host_info . "\n";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>WIFIBOTPROYECT</title>
+	<link rel="icon" type="image/png" href="images/chip.png"/>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="team" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/owl.carousel.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-style.css">
+
+</head>
+<body>
+
+     <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+
+
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="index.html" class="navbar-brand">WiFi-Bots Proyect</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                         <li><a href="#home" class="smoothScroll">Inicio</a></li>
+                         <li><a href="#feature" class="smoothScroll">Características</a></li>
+                         <li><a href="#about" class="smoothScroll">Acerca de</a></li>
+                         <li><a href="footer" class="smoothScroll">Comienza</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="#"><span>MESHROBOTIX@GMAIL.COM</span></a></li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
+    
+
+     <!-- FEATURE -->
+
+     
+     <section id="home2" data-stellar-background-ratio="0.5">
+          
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-offset-3 col-md-6 col-sm-12">
+                         <div class="home2-info">
+                              <h3>MECH ROBOTIX</h3>
+                              <h1>Desarrolla habilidades tecnológicas!</h1>
+                              
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+    
+
+    <section>
+        <div class="remote">
+            <div><img src="images/cuadro.png"></div>
+            <div><img src="images/arriba.png"></div>
+            <div><img src="images/cuadro.png"></div>
+            <div><img src="images/izquierda.png"></div>
+            <div><img src="images/centro.png"></div>
+            <div><img src="images/derecha.png"></div>
+            <div><img src="images/cuadro.png"></div>
+            <div><img src="images/abajo.png"></div>
+            <div><img src="images/cuadro.png"></div>
+        </div>
+    </section>
+
+    
+
+     <!-- FOOTER -->
+     <footer id="footer" data-stellar-background-ratio="0.5">
+          
+          <div class="container">
+               <div class="row">
+                   
+                    
+                    <div class="copyright-text col-md-12 col-sm-12">
+                         <div class="col-md-6 col-sm-6">
+                              <p>Copyright &copy; Todos los Derechos Están Reservados.</p>
+                         </div>
+
+                         <div class="col-md-6 col-sm-6">
+                              <ul class="social-icon">
+                                   <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                   <li><a href="#" class="fa fa-twitter"></a></li>
+                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                              </ul>
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </footer>
+
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
